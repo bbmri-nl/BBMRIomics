053c3c520cee48f5fd0fc41c52083e41838484d3
mvaniterson <m.van_iterson@lumc.nl>
2017-05-30 16:07:49 +0100

    redirect all genotyping output to log-file and add the SNPs 50 list

--- BBMRIomics/inst/scripts/genotyping.sh
+++ BBMRIomics/inst/scripts/genotyping.sh
@@ -6,6 +6,10 @@
 RSCRIPT=/usr/lib64/microsoft-r/3.3/lib64/R/bin/Rscript
 OUT=/virdir/Backup/RP3_analysis/SwapDetection/
 
+# write everything to genotype.log
+
+(
+    
 ##intra
 $RSCRIPT genotyping.R --typex HRC --cohort ALL --out ${OUT} --filex ${OUT}/HighQualPositions.GCRh37.bed
 $RSCRIPT genotyping.R --typex GoNL --cohort ALL --out ${OUT} --filex ${OUT}/HighQualPositions.GCRh37.bed
@@ -18,3 +22,11 @@ $RSCRIPT genotyping.R --typex RNA --typey GoNL --cohort ALL --out ${OUT} --filex
 $RSCRIPT genotyping.R --typex DNAm --typey HRC --cohort ALL --out ${OUT}
 $RSCRIPT genotyping.R --typex DNAm --typey GoNL --cohort ALL --out ${OUT}
 $RSCRIPT genotyping.R --typex HRC --typey GoNL --cohort ALL --filex ${OUT}/HighQualPositions.GCRh37.bed --out ${OUT}
+
+
+OUT=/virdir/Backup/RP3_analysis/SwapDetection/SNP50
+$RSCRIPT genotyping.R --typex RNA --typey HRC --cohort ALL --out ${OUT} --filex ${OUT}/output_RNA_50.vcf
+$RSCRIPT genotyping.R --typex RNA --typey GoNL --cohort ALL --out ${OUT} --filex ${OUT}/output_RNA_50.vcf
+$RSCRIPT genotyping.R --typex RNA --cohort ALL --out ${OUT} --filex ${OUT}/output_RNA_50.vcf
+
+) > genotyping.log 2>&1
